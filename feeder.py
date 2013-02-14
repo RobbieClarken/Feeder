1a387292545df23ef6bd47a0d59b28337b462e1b
RobbieClarken <rob.clarken@gmail.com>
2013-02-14 19:12:50 +1100

    Documented Feed class and made it accept more named parameters

--- feeder.py
+++ feeder.py
@@ -110,8 +110,46 @@ class Category(Element):
 class Feed(Element):
     """Generates an Atom feed based on the specification described at
        http://www.atomenabled.org/developers/syndication/
+       Required elements:
+       * id: Identifies the feed using a universally unique and
+         permanent URI. If you have a long-term, renewable lease on
+         your Internet domain name, then you can feel free to use
+         your website's address.
+       * title: Contains a human readable title for the feed. Often the
+         same as the title of the associated website. This value should
+         not be blank.
+       * updated: Indicates the last time the feed was modified in a
+         significant way.
+       Recommended elements:
+       * author: Names one author of the feed. A feed may have multiple
+         author elements. A feed must contain at least one author
+         element unless all of the entry elements contain at least one
+         author element. See Author class.
+       * link: Identifies a related Web page. The type of relation is
+         defined by the rel attribute. A feed is limited to one
+         alternate per type and hreflang. A feed should contain a link
+         back to the feed itself. See Link class.
+       Optional elements:
+       * category: Specifies a category that the feed belongs to. A
+         feed may have multiple category elements. See Category class.
+       * contributo: Names one contributor to the feed. An feed may
+         have multiple contributor elements.
+       * generator: Identifies the software used to generate the feed,
+         for debugging and other purposes. Both the uri and version
+         attributes are optional.
+       * icon: Identifies a small image which provides iconic visual
+         identification for the feed. Icons should be square.
+       * logo: Identifies a larger image which provides visual
+         identification for the feed. Images should be twice as wide as
+         they are tall.
+       * rights: Conveys information about rights, e.g. copyrights, held
+         in and over the feed.
+       * subtitle: Contains a human-readable description or subtitle for
+         the feed.
     """
-    def __init__(self, id, title, updated=None):
+    def __init__(self, id, title, updated=None, authors=[], links=[],
+                 categories=[], contributors=[], generator=None,
+                 icon=None, logo=None, rights=None, subtitle=None):
         #TODO: kwargs to be appended to super initialisation
         super(Feed, self).__init__('feed',
                                    {'xlmns': 'http://www.w3.org/2005/Atom'})
@@ -139,14 +177,14 @@ class Feed(Element):
         self.updated = Element('updated')
         self.updated.text = updated.isoformat()
         # Recommended
-        self.authors = [] # List of Person objects
-        self.link = None
+        self.authors = authors
+        self.links = links
         # Optional
-        self.categories = [] # List of Person objects
-        self.contributors = [] # List of Category objects
-        self.generator = None
-        self.icon = None
-        self.logo = None
-        self.rights = None
-        self.subtitle = None
+        self.categories = categories
+        self.contributors = contributors # List of Category objects
+        self.generator = generator
+        self.icon = icon
+        self.logo = logo
+        self.rights = rights
+        self.subtitle = subtitle
 
